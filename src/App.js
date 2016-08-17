5bd6710c53cd9bf583453245ea580a887ef12745
Praveen Prasad <veen.edu@gmail.com>
2016-08-17 18:56:15 +0530

    Add redux and react redux

--- src/App.js
+++ src/App.js
@@ -6,14 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          Hello, I am new.
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        Hello, all set up done!!
       </div>
     );
   }
