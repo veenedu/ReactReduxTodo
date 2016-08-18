b9cb0975f3985d58f428809840320e07554e2a99
Praveen Prasad <veen.edu@gmail.com>
2016-08-18 15:49:42 +0530

    Wrap App inside redux provider

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import './App.css';
 import TodoForm from './components/TodoForm.react'
 import TodoList from './components/TodoList.react'
 import store from './redux/store';
+import { Provider } from 'react-redux';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <TodoForm />
-        <TodoList tasks = {[{id:1,title:'MyTask'}]} />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <TodoForm />
+          <TodoList tasks = {[{id:1,title:'MyTask'}]} />
+        </div>
+      </Provider>
     );
   }
 }
